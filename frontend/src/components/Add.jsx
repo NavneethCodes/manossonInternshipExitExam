36b9fe5d242be5ccb13a543090df57716f971ef3
NavneethCodes <navneetharun04@gmail.com>
2024-08-11 12:18:14 +0530

    changes

--- frontend/src/components/Add.jsx
+++ frontend/src/components/Add.jsx
@@ -1,89 +1,88 @@
 import { Box, Button, TextField } from "@mui/material";
-import  { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Add = () => {
   const navigate = useNavigate();
-  var [inputs, setInputs] = useState({
+  const [inputs, setInputs] = useState({
     EmpName: "",
     designation: "",
-    empId:"",
+    empId: "",
     img_url: ""
   });
+
   const inputHandler = (e) => {
     console.log(e.target.value);
     setInputs({ ...inputs, [e.target.name]: e.target.value });
-    console.log("in",inputs);
+    console.log("in", inputs);
   };
+
   const addData = () => {
-    axios.post("hhtp://localhost:3001/add", inputs).then((response) => {
-      console.log("Data add successfully!", response);
-      navigate("/");
-    })
-    .catch((error)=> {
-      console.error("Error fetching the data!", error);
-    })
+    axios.post("http://localhost:3001/add", inputs)
+      .then((response) => {
+        console.log("Data added successfully!", response);
+        navigate("/");
+      })
+      .catch((error) => {
+        console.error("Error fetching the data!", error);
+      });
   };
+
   return (
     <div>
-      <div>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "90vh",
+        }}
+      >
         <Box
+          component="form"
           sx={{
             display: "flex",
             flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "90vh",
+            gap: 2,
+            width: "600px",
           }}
         >
-          <Box
-            component="form"
-            sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 2,
-              width: "600px",
-            }}
-          >
-            <TextField
-              variant="outlined"
-              placeholder="Employee Name"
-              onChange={inputHandler}
-              name="EmpName"
-              value={inputs.title}
-              fullWidth
-            />
-            <TextField
-              variant="outlined"
-              placeholder="Designation"
-              onChange={inputHandler}
-              name="designation"
-              value={inputs.designation}
-              multiline={4}
-            />
-             <TextField
-              variant="outlined"
-              placeholder="Employee Id"
-              onChange={inputHandler}
-              name="empId"
-              value={inputs.empId}
-            />
-            <TextField
-              variant="outlined"
-              placeholder="Photo(paste any link from the browser)"
-              onChange={inputHandler}
-              name="img_url"
-              value={inputs.img_url}
-            />
-           
-
-            <Button variant="contained" color="secondary" onClick={addData}>
-              Submit
-            </Button>
-          </Box>
+          <TextField
+            variant="outlined"
+            placeholder="Employee Name"
+            onChange={inputHandler}
+            name="EmpName"
+            value={inputs.EmpName}
+            fullWidth
+          />
+          <TextField
+            variant="outlined"
+            placeholder="Designation"
+            onChange={inputHandler}
+            name="designation"
+            value={inputs.designation}
+          />
+          <TextField
+            variant="outlined"
+            placeholder="Employee Id"
+            onChange={inputHandler}
+            name="empId"
+            value={inputs.empId}
+          />
+          <TextField
+            variant="outlined"
+            placeholder="Photo (paste any link from the browser)"
+            onChange={inputHandler}
+            name="img_url"
+            value={inputs.img_url}
+          />
+          <Button variant="contained" color="secondary" onClick={addData}>
+            Submit
+          </Button>
         </Box>
-      </div>
+      </Box>
     </div>
   );
 };
